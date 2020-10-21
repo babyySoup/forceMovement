da1a0828cdd160d6ae9d35bc8e7674bcf24925c4
babyySoup <71286763+babyySoup@users.noreply.github.com>
2020-10-20 19:17:11 -0700

    : > force

---
+++ force！/Assets/ForceCube.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceCube : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float jumpForce = 1000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        rb.AddForce(Vector3.up * jumpForce);
+    }
+}
---
+++ force！/Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody playerBody;
+
+
+    private Vector3 inputVector;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+    inputVector = new Vector3(Input.GetAxis("Horizontal") * 10f, playerBody.velocity.y, Input.GetAxis("Vertical") * 10f);
+    transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
+  
+
+    }
+    private void FixedUpdate()
+    {
+        playerBody.velocity = inputVector;
+    }
+}
